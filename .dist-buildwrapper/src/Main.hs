38cb049772b1e51a9ca8ab54fee3f83b171cec21
Camila Brondani <chbrondani@inf.ufsm.br>
2012-11-19 23:00:39 -0200

    Terceiro Commit

--- .dist-buildwrapper/src/Main.hs
+++ .dist-buildwrapper/src/Main.hs
@@ -1,4 +1,82 @@
 module Main where
 
-main::IO()
-main = undefined
+import Text.Printf 
+
+type Point     = (Float,Float)
+type Color     = (Int,Int,Int)
+type Circle    = (Point,Float)
+
+imageWidth :: Int
+imageWidth = 500
+
+imageHeight :: Int
+imageHeight = 500
+
+
+-- Funcao principal que faz leitura do dataset e gera arquivo SVG
+main :: IO ()
+main = do 
+        strcontent <- readFile infile
+        let pairs = map (span (/= ' ')) (lines strcontent)
+            freqs = readInts (map snd pairs)
+        writeFile outfile (svgCloudGen imageWidth imageHeight freqs)
+        putStrLn "Ok!"
+        where 
+                infile = "dataset.txt"
+                outfile = "tagcloud.svg"
+
+
+-- Transforma lista de strings em lista de inteiros
+readInts :: [String] -> [Int]
+readInts ss = map read ss
+
+
+-- Gera o documento SVG da tag cloud, concatenando cabecalho, conteudo e rodape
+svgCloudGen :: Int -> Int -> [Int] -> String
+svgCloudGen w h dataset = 
+        "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" ++ 
+        "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n" ++
+        (svgViewBox w h) ++
+        (concat (svgBubbleGen w h dataset)) ++ "</svg>\n"
+
+
+-- Esta funcao deve gerar a lista de circulos em formato SVG.
+-- A implementacao atual eh apenas um teste que gera um circulo posicionado no meio da figura.
+-- TODO: Alterar essa funcao para usar os dados do dataset.
+
+dadosDataset :: IO()
+dadosDataset = do 
+        strcontent <- readFile "dataset.txt"
+        let pairs = map (span (/= ' ')) (lines strcontent)
+            freqs = readInts (map snd pairs)
+        print freqs
+        
+svgBubbleGen:: Int -> Int -> [Int] -> [String]
+svgBubbleGen w h dataset = [svgCircle ((fromIntegral w/2, fromIntegral h/2), fromIntegral 2249/50)]
+                --where dataset = dadosDataset
+
+
+-- Gera string representando um circulo em SVG. A cor do circulo esta fixa. 
+-- TODO: Alterar esta funcao para mostrar um circulo de uma cor fornecida como parametro.
+
+--funcCor :: Int -> Int -> Int -> Color
+--funcCor r g b = (r,g,b)
+  --      where r >= 0 && r <=255 
+    --          g >= 0 && g <=255 
+      --        b >= 0 && b <=255 
+
+--funcEspiral :: Int -> Int -> Int -> Float
+--funcEspiral x y a  x = a*5*(cos 5) && y = a*5*(sin 5)
+
+
+svgCircle :: Circle -> String
+svgCircle ((x,y),raio) = printf "<circle cx=\"50\" cy=\"40\" r=\"90\" fill=\"rgb(0,0,255)\" />\n <circle cx=\"50\" cy=\"90\" r=\"80\" fill=\"rgb(255,0,0)\" /> <circle cx=\"10\" cy=\"80\" r=\"20\" fill=\"rgb(0,255,0)\" />"
+
+
+
+-- Configura o viewBox da imagem e coloca retangulo branco no fundo
+svgViewBox :: Int -> Int -> String
+svgViewBox w h =
+        printf "<svg width=\"%d\" height=\"%d\" viewBox=\"0 0 %d %d\"" w h w h ++ 
+                " version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n" ++
+        printf "<rect x=\"0\" y=\"0\" width=\"%d\" height=\"%d\" style=\"fill:white;stroke:purple;stroke-width:4\"/>\n" w h
