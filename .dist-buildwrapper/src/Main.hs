a01745061a73c40e3fe0827a4ab1808c73ff958e
Camila Brondani <chbrondani@inf.ufsm.br>
2012-11-24 18:28:25 -0200

    Oitavo Commit: Testando as funcoes separadamente - verifica se possui interseccao ok

--- .dist-buildwrapper/src/Main.hs
+++ .dist-buildwrapper/src/Main.hs
@@ -6,7 +6,7 @@ import Data.List
 
 type Point     = (Float,Float)
 type Color     = (Int,Int,Int)
-type Circle    = (Point,Float,Color)
+type Circle    = (Point,Float)
 
 imageWidth :: Int
 imageWidth = 500
@@ -48,7 +48,7 @@ svgCloudGen w h dataset =
 -- A implementacao atual eh apenas um teste que gera um circulo posicionado no meio da figura.
 -- TODO: Alterar essa funcao para usar os dados do dataset.
 svgBubbleGen:: Int -> Int -> [Int] -> [String]
-svgBubbleGen w h dataset = [funcJunta (fromIntegral w/2) (fromIntegral h/2) (reverse (sort dataset)) (0,255,0)]
+svgBubbleGen w h dataset = [funcJunta (fromIntegral w/2) (fromIntegral h/2) (reverse (sort dataset))]----funcao reverse: cria uma nova sequência do original com itens na ordem inversa e funcao sort: serve para ordenar
         --where raio = funcRaio dataset
         
         
@@ -59,13 +59,12 @@ funcRaio dataset = (fromIntegral (head dataset)/20) : funcRaio (tail dataset) --
 
 
 -- Função que gera os circulos do tipo Circle ((x,y),raio,(r,g,b))
-funcJunta :: Float -> Float -> [Int] -> Color -> String
-funcJunta  _ _ [] _ = []
-funcJunta x y dataset (_) = svgCircle ((x,y), raio ,(0,255,0)) ++ (funcJunta ponto_x ponto_y (tail dataset) (255,0,0))
-        where
-        ponto_x = x*0.1*(cos 0.1)
-        ponto_y = y*0.1*(sin 0.1)
-        raio = fromIntegral (head dataset)/20
+funcJunta :: Float -> Float -> [Int] -> String
+funcJunta  _ _ [] = []
+funcJunta x y dataset = svgCircle ((x,y), raio) ++ (funcJunta ponto_x ponto_y (tail dataset))
+        where ponto_x = x*0.1*(cos 0.1)
+              ponto_y = y*0.1*(sin 0.1)
+              raio = fromIntegral (head dataset)/20
         
 
 -- Gera string representando um circulo em SVG. A cor do circulo esta fixa. 
@@ -79,7 +78,7 @@ lisCircle [] = []
 lisCircle (h:t) = svgCircle (h) : lisCircle t 
 
 svgCircle :: Circle -> String
-svgCircle ((x,y),raiocirc,(r,g,b)) = printf "<circle cx=\"%f\" cy=\"%f\" r=\"%f\" fill=\"rgb(%d,%d,%d)\" />\n" x y raiocirc r g b
+svgCircle ((x,y),raiocirc) = printf "<circle cx=\"%f\" cy=\"%f\" r=\"%f\" fill=\"rgb(%d,%d,%d)\" />\n" x y raiocirc
 
 
 -- Configura o viewBox da imagem e coloca retangulo branco no fundo
@@ -89,15 +88,21 @@ svgViewBox w h =
                 " version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n" ++
         printf "<rect x=\"0\" y=\"0\" width=\"%d\" height=\"%d\" style=\"fill:white;stroke:purple;stroke-width:4\"/>\n" w h
         
-        
--- Esta função calcula a distância entre 2 círculos
---funcDistancia :: Circle -> Circle -> Float
---funcDistancia ((x1,y1),r1,_) ((x2,y2),r2,_) = sqrt (((x2-x1)^2)+((y2-y1)^2))
 
 -- Esta função verifica se 2 círculos possuem intersecção
 funcInterseccao :: Circle -> Circle -> Bool
-funcInterseccao ((x1,y1),r1,_)((x2,y2),r2,_)
-        |distancia > r1+r2        = False
-        |distancia < r1-r2        = False
-        |distancia == 0 && r1==r2 = True
-        where distancia = sqrt (((x2-x1)^2)+((y2-y1)^2))
+funcInterseccao ((x1,y1),r1) ((x2,y2),r2)
+        |distancia > r1+r2 = False
+        |otherwise         = True
+        where distancia = sqrt (xC+yC)
+              xC = (x2 - x1) ^ 2 
+              yC = (y2 - y1) ^ 2 
+              
+-- Esta função coloca um ponto na espiral que não tenha intersecção
+funcEspiral :: Circle -> Circle -> Float -> Float-> (Circle, Float) -- (Circle, Float): o Circle corresponde ao círculo que será criado na espiral e o Float é o t.
+funcEspiral ((x,y),r) (ponto,novoR) a t
+        |k == True = funcEspiral ((x,y),r) ((novoX,novoY),novoR) a (t + (0.03)) -- Se tem interseccao, é necessário deslocar este ponto, alterando o t.
+        |otherwise = (((novoX, novoY),novoR),t)
+        where k = funcInterseccao ((x,y),r) ((novoX,novoY),novoR) 
+              novoX = novoX + (a*t*(cos t))
+              novoY = novoY + (a*t*(sin t))
